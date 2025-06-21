589ba3ee85d923d4165bfe1f97d818bf2b33b3e3
Jeff Ryan <jefrya123@gmail.com>
2025-06-21 19:40:22 -0400

    Fixed pylint issues now 10/10

--- data_import.py
+++ data_import.py
@@ -1,23 +1,26 @@
-"""Loading the data from titantic"""
+"""Loading the data from Titanic CSV into a SQLite database."""
 
-import pandas as pd
 import sqlite3
+import pandas as pd
+
 
 def load_csv(filepath):
-    """Load data from CSV to DF"""
+    """Load data from CSV to a DataFrame."""
     return pd.read_csv(filepath)
 
-def save_sqlite(df, db_name, table_name):
-    """Save DF to sqlite DB"""
+
+def save_sqlite(dataframe, db_name, table_name):
+    """Save DataFrame to a SQLite3 database."""
     conn = sqlite3.connect(db_name)
-    df.to_sql(table_name, conn, if_exists='replace', index=False)
+    dataframe.to_sql(table_name, conn, if_exists='replace', index=False)
     conn.close()
 
+
 if __name__ == "__main__":
-    # Load  from CSV
-    df = load_csv('titanic.csv')
-    
-    # Save the df to DB
-    save_sqlite(df, 'titanic.db', 'titanic_data')
-    
+    # Load from CSV
+    titanic_df = load_csv('data/titanic.csv')
+
+    # Save the DataFrame to DB
+    save_sqlite(titanic_df, 'titanic.db', 'titanic_data')
+
     print("Data loaded and saved.")
