86164b799b05ce1fae75001df6ce86aabd7be757
Jeff Ryan <jefrya123@gmail.com>
2025-07-01 14:25:27 -0400

    Added initial streamlit home page

---
+++ streamlit_app/home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def main():
+    st.title("Titanic Dashboard")
+    st.markdown("""
+                Titanic Capstone Project
+    """)
+
+if __name__ == "__main__":
+    main()
