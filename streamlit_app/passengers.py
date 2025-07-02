ed422ba15c541f544ac5560241cb5fc1b79a9ed8
Jeff Ryan <jefrya123@gmail.com>
2025-07-01 20:09:53 -0400

    First Passenger Viewing Streamlit

---
+++ streamlit_app/passengers.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import requests
+import pandas as pd
+
+st.set_page_config(page_title="Passenger List")
+st.title("Passenger List")
+
+# Getting Data from API
+try:
+    response = request.get("http://localhost:5000/passengers")
+    if response.status_code == 200:
+        passengers = response.json()
+        df = pd.DataFrame(passengers)
+        st.dataframe(df)
+    else:
+        st.error("Failed to retieve from API.")
+    except requests.expections.ConnectionError:
+        st.error("Flask API is not running.")
+    
