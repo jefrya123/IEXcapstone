80a84b84e042f81fcc248ad92c1942b120568756
Jeff Ryan <jefrya123@gmail.com>
2025-07-07 18:10:27 -0400

    Added Divider

--- streamlit_app/pages/2_survival_prediction.py
+++ streamlit_app/pages/2_survival_prediction.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pickle
 import numpy as np
 import os
-import pickle
 
 st.set_page_config(page_title="Titanic Survival Prediction")
 st.title("Titanic Survival Prediction")
+st.markdown("---")
 
 # Load model
 
@@ -15,22 +15,27 @@ with open(model_path, "rb") as f:
     model = pickle.load(f)
 
 # Form for user input
-st.subheader("Enter passenger information:")
 
-pclass = st.selectbox("Passenger Class", [1, 2, 3])
-sex = st.selectbox("Sex", ["male", "female"])
-age = st.slider("Age", 1, 80, 25)
-sibsp = st.number_input("Siblings/Spouses Aboard", min_value=0, max_value=10, value=0)
-parch = st.number_input("Parents/Children Aboard", min_value=0, max_value=10, value=0)
 
-# Convert categorical input
-sex_encoded = 1 if sex == "male" else 0
+with st.form("Prediction Form"):
+    st.subheader("Enter passenger information:")
+    pclass = st.selectbox("Passenger Class", [1, 2, 3])
+    sex = st.selectbox("Sex", ["Male", "Female"])
+    age = st.slider("Age", 1, 80, 25)
+    sibsp = st.slider("Siblings/Spouses Aboard", 0, 10, 0)
+    parch = st.slider("Parents/Children Aboard", 0, 10, 0)
+ 
+    submitted = st.form_submit_button("Predict Survival")
 
-# Predict
-if st.button("Predict Survival"):
+
+if submitted:
+
+    sex_encoded = 1 if sex == "Male" else 0
+
     features = np.array([[pclass, sex_encoded, age, sibsp, parch]])
     prediction = model.predict(features)[0]
+
     if prediction == 1:
-        st.success("The passenger would have SURVIVED!")
+        st.success("The passenger is predicted to **survive**! 🎉")
     else:
-        st.error("The passenger would NOT have survived.")
+        st.error("The passenger is predicted to **not survive**. 😢")
